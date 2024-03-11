87a7872d1ccf0aa788868d1f8cddcc71f2853a4f
unknown <aalimomen110@gmail.com>
2024-03-11 10:56:44 +0330

    Initial: Our base backend project

---
+++ GourmetSolution/Gourmet.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gourmet.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public string SignUp()
+        {
+            return "Hello World";
+        }
+    }
+}
