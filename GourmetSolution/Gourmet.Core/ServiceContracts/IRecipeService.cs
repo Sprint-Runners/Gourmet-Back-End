3cf2cb04abe16eb3cf310dd8cc6c91cdaaf6fa94
HengamehBaghiyan <hengamehb1382@gamil.com>
2024-04-18 16:51:06 +0330

    fix: fixing some response for HomePage

--- GourmetSolution/Gourmet.Core/ServiceContracts/IRecipeService.cs
+++ GourmetSolution/Gourmet.Core/ServiceContracts/IRecipeService.cs
@@ -1,4 +1,5 @@
-﻿using Gourmet.Core.Domain.Other_Object;
+﻿using Gourmet.Core.Domain.Entities;
+using Gourmet.Core.Domain.Other_Object;
 using Gourmet.Core.DTO.Request;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace Gourmet.Core.ServiceContracts
     {
         Task<RecipeResponse> CreateRecipeByChef(AddRecipeRequest request, string userId, string username);
         Task<InCompleteRecipeResponse> CreateInCompleteRecipe(AddRecipeRequest request, string userId, string username);
+        Task<IEnumerable<Recipe>> Get_All_Recipe();
     }
 }
